204ddc13fd4302f4bd967267e06965be586c8b5f
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-05-14 15:16:23 +0200

    MILESTONE 0

--- js/main.js
+++ js/main.js
@@ -1,2 +1,42 @@
 'use strict'
 console.log('eccomi')
+//Creare l’array di oggetti con le informazioni fornite
+const myArrayTeams = [
+    {
+        nome: 'Wayne Barnett',
+        ruolo: 'Fouder e CEO',
+        img: '',
+    },
+
+    {
+        nome: 'Angela Caroli',
+        ruolo: 'Chief Editor',
+        img: '',
+    },
+
+    {
+        nome: 'Walter Gordon',
+        ruolo: 'Manager',
+        img: '',
+    },
+
+    {
+        nome: 'Angela Lopez',
+        ruolo: 'Media Menager',
+        img: '',
+    },
+
+    {
+        nome: 'Scot Estrada',
+        ruolo: 'Developer',
+        img: '',
+    },
+
+    {
+        nome: 'Barbara Ramon',
+        ruolo: 'Grafica',
+        img: '',
+    },
+];
+
+console.log(myArrayTeams)
