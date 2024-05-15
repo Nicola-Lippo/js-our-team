d9db17b70c8df12158b538d60b96926d392006c6
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-05-15 15:13:26 +0200

    Bonus 1 + correzione

--- js/main.js
+++ js/main.js
@@ -6,41 +6,34 @@ const myArrayTeams = [
     {
         nome: 'Wayne Barnett',
         ruolo: 'Fouder e CEO',
-        img: '',
+        img: 'wayne-barnett-founder-ceo.jpg',
     },
-
     {
         nome: 'Angela Caroli',
         ruolo: 'Chief Editor',
-        img: '',
+        img: 'angela-caroll-chief-editor.jpg',
     },
-
     {
         nome: 'Walter Gordon',
         ruolo: 'Manager',
-        img: '',
+        img: 'walter-gordon-office-manager.jpg',
     },
-
     {
         nome: 'Angela Lopez',
         ruolo: 'Media Menager',
-        img: '',
+        img: 'angela-lopez-social-media-manager.jpg',
     },
-
     {
         nome: 'Scot Estrada',
         ruolo: 'Developer',
-        img: '',
+        img: 'scott-estrada-developer.jpg',
     },
-
     {
         nome: 'Barbara Ramon',
         ruolo: 'Grafica',
-        img: '',
+        img: 'barbara-ramos-graphic-designer.jpg',
     },
 ];
-//MILESTONE 1
-//Stampare su console, per ogni membro del team, le informazioni
 console.log(myArrayTeams);
 //MILESTONE 2
 //Stampare le stesse informazioni su DOM sottoforma di stringhe
@@ -48,6 +41,8 @@ const ul = document.querySelector('.team');
 for (let i = 0; i < myArrayTeams.length; i++) {
     //creo una variabile per leggere i valori degli oggetti
     const attributi = myArrayTeams[i];
+    //MILESTONE 1
+    //Stampare su console, per ogni membro del team, le informazioni
     console.log(attributi)
     const li = document.createElement('li');
     //stampo nome
@@ -60,9 +55,15 @@ for (let i = 0; i < myArrayTeams.length; i++) {
     div.append(attributi.ruolo);
     li.append(div);
     ul.append(li);
-    //stampo foto
-    const img = document.createElement('img');
-    img.append(attributi.img);
-    li.append(img);
+    //stampo foto inn console
+    const p = document.createElement('p');
+    p.append(attributi.img);
+    li.append(p);
     ul.append(li);
+    //BONUS 1
+    //stampo foto nella pagina HTML
+    const imgPersona = document.createElement('img');
+    imgPersona.src = 'img/' + attributi.img;
+    li.append(imgPersona)
+
 }
